1582f18e6effdf774136c3a24510cf0df1561e8e
Wolfgirl410 <159813442+Wolfgirl410@users.noreply.github.com>
2025-06-03 23:08:19 +0800

    added variables

--- Printing Story on Console.cpp
+++ Printing Story on Console.cpp
@@ -3,10 +3,38 @@
 using namespace std;
 
 int main() {
-	cout<<"On a peaceful night,the tiny,icelandic village whispered to the dancing trees...\n\nYou, um...what is your name...?";
-	cout<<"\n\nAh, that is a powerful name...A name from a Myth...";
-	cout<<"\n\nOh...You are him...and you have come to save us!";
-	cout<<"\n\nWhy do I know you? Well a great prophecy stated that you would save us from this demon terrorizing our weathers and ruining our crops...Dnmairy";
-	cout<<"\n\nStill don't believe me? Come, we must get to the Scrolls of History!\n\n";
+	int playerHealth;
+	int livesCount;
+	int PlayerId;
+	float winPercentage;
+	bool playerWon;
+	int typePlayer;
 
+	cout<<"On a peaceful night,the tiny,icelandic village whispered to the dancing trees...\n\nYou, um...what is your name...?\n\n";
+
+	cout<<"Enter your player ID here: ";
+	cin>>PlayerId;
+
+	cout<<"Enter number of lives you would like to start with: ";
+	cin>>livesCount;
+	
+	cout<<"Enter your health that you would like to start with: ";
+	cin>>playerHealth;
+
+	cout<<"Enter your percentage of winning: ";
+	cin>>winPercentage;
+
+	cout<<"Has the player won? (1 for Yes, 0 for No): ";
+	cin>>playerWon;
+	
+	cout<<"Enter a type of player (A/B/C/D): ";
+	cin>>typePlayer;
+
+	cout<<"Your player ID is "<<PlayerId<<"\n\n";
+	cout<<"The number of lives you will have is "<<livesCount<<"\n\n";
+	cout<<"Your health is "<<playerHealth<<"\n\n";
+	cout<<"Your win percentage is "<<winPercentage<<"\n\n";
+	cout << "Has Won         : " << (playerWon ? "Yes" : "No") << endl;
+	cout << "Player Type     : " << typePlayer << endl;
+	return 0;
 }
