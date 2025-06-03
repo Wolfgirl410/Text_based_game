7b5b61f16ece9a42ab89383f9a86e7324eb3a6b0
Wolfgirl410 <159813442+Wolfgirl410@users.noreply.github.com>
2025-06-03 11:55:50 +0800

    for part 1

--- Printing Story on Console.cpp
+++ Printing Story on Console.cpp
@@ -1,4 +1,3 @@
-#include <bits/stdc++.h>
 #include <iostream>
 
 using namespace std;
@@ -10,20 +9,4 @@ int main() {
 	cout<<"\n\nWhy do I know you? Well a great prophecy stated that you would save us from this demon terrorizing our weathers and ruining our crops...Dnmairy";
 	cout<<"\n\nStill don't believe me? Come, we must get to the Scrolls of History!\n\n";
 
-	int playerHealth;
-	int livesCount;
-	char PlayerId[25];
-	int winPercentage;
-	bool playerWon;
-	int typePlayer;
-
-	cout<<"Enter your name here: ";
-	cin>>PlayerId;
-
-	cout<<"Enter number of Lives you would like to start with: ";
-	cin>>livesCount;
-	
-	cout<<"Enter your health that you would like to start with: ";
-	cin>>playerHealth;
-	return 0;
 }
